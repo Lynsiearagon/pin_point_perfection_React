0c4f15092d0f0d4cd949c1ce331d9c17a4268550
Lynsie Aragon <lynsie.aragon@gmail.com>
2023-06-19 19:31:19 -0400

    Add bowling balls and oil patterns objects

---
+++ src/data/bowlingBallList.js
@@ -0,0 +1,78 @@
+const BowlingBallList = {
+
+    "storm axiom pearl": {
+        "name": "Storm Axiom Pearl",
+        "coverstock": "NeX Pearl Reactive",
+        "finish": "1500-grit Polish",
+        "condition": "Medium-Heavy",
+        "url": ""
+    },
+
+    "storm proton physix 4": {
+        "name": "Storm Proton Physix 4",
+        "coverstock": "NeX Solid Reactive",
+        "finish": "2000-grit",
+        "condition": "Heavy",
+        "url:": ""
+    },
+    
+    "storm pitch black": {
+        "name": "Storm Pitch Black",
+        "coverstock": "Control Urethane Solid",
+        "finish": "1000-grit",
+        "condition": "Light",
+        "url:": ""
+    },
+
+    "roto grip hyped solid": {
+        "name": "Roto Grip Hyped Solid",
+        "coverstock": "VTC Solid Reactive",
+        "finish": "3000-grit",
+        "condition": "Medium",
+        "url:": ""
+    }, 
+
+    "roto grip exotic gem": {
+        "name": "Roto Grip Exotic GEM",
+        "coverstock": "Micro Trax Pearl Reactive",
+        "finish": "Reacta Gloss",
+        "condition": "Medium-Heavy",
+        "url:": ""
+    },
+    
+    "roto grip hustle wine": {
+        "name": "Roto Grip Hustle Wine",
+        "coverstock": "VTC Pearl Reactive",
+        "finish": "Reacta Gloss",
+        "condition": "Light-Medium",
+        "url:": ""
+    }, 
+
+    "900 global reality": {
+        "name": "900 Global Reality",
+        "coverstock": "S84 Response Solid",
+        "finish": "2000 grit",
+        "condition": "Heavy",
+        "url:": ""
+    },
+
+    "900 global burner pearl": {
+        "name": "900 Global Burner Pearl",
+        "coverstock": "S62 Pearl",
+        "finish": "Reacta Gloss",
+        "condition": "Light-Medium",
+        "url:": ""
+    },
+
+    "hammer obsession tour": {
+        "name": "Hammer Obsession Tour",
+        "coverstock": "TourV2 Solid",
+        "finish": "500, 1000 Siaair Micro Pad",
+        "condition": "Medium-Heavy",
+        "url:": ""
+    }
+
+}
+
+export default BowlingBallList;
+
---
+++ src/data/oilPatternList.js
@@ -0,0 +1,29 @@
+const OilPatternList = {
+
+    "boardwalk": {
+      "name": "Boardwalk (50 UL)",
+      "distance": "35 feet",
+      "volume": "23.9 mL",
+      "length": "Short",
+      "url": ""
+    },
+  
+    "eiffel tower": {
+      "name": "Eiffel Tower",
+      "distance": "48 feet",
+      "volume": "25.95 mL",
+      "length": "Long",
+      "url": ""
+    },
+  
+    "red square": {
+      "name": "Red Square",
+      "distance": "40 feet",
+      "volume": "25.16",
+      "length": "Medium",
+      "url": ""
+    }
+  
+}
+  
+export default OilPatternList;
